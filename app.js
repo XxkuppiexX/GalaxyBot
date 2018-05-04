ff412c3c9ae5f63c6f033e4fa0fdf0147e9d1d82
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2018-05-04 13:00:48 +0200

    join msg

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const config = require("./config.json");
 // config.token contains the bot's token
 // config.prefix contains the message prefix.
 
-bot.on('guildMemberAdd', member => {
+client.on('guildMemberAdd', member => {
     member.guild.channels.get('428835187456213013').send(' Welkom in de ruimte,' + member.user.username + '!'); 
 });
 
