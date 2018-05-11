9ae44745d7f9459e6d93d803cb2853293214b350
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2018-05-11 20:19:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -77,6 +77,61 @@ if (message.content.toLowerCase().startsWith(prefix + `sluit`)) {
 });
 
 
+function clean(text) {
+    if (typeof(text) === "string")
+      return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
+    else
+        return text;
+}
+var prefix = "!";
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+if (message.content.toLowerCase().startsWith(prefix + `lang eng`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (message.guild.channels.exists("name", "prices")) return message.channel.send(`you have alredy open the english chats`);
+    message.guild.createChannel(`prices`, "text").then(c => {
+        let role = message.guild.roles.find("name", "⚙️ Support ⚙️");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: The english channels had geen created`);
+        c.send(":earth_americas: `GalaxyBot` :earth_americas: \n \n `Prijzen` \n \n `*DC bot {small}*` €1,-\n `*DC bot {medium}*` €2,-\n `*DC bot {big}*` €3,- \n \n `PAY ATTENTION! coming soon minecraft servers!`
+
+comet️ @everyone comet️")
+if (message.content.toLowerCase().startsWith(prefix + `sluit`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`je kan het ticket niet sluiten als je niet in de channel van de ticket zit.`);
+
+    message.channel.send(`Weet je het zeker? Deze actie kan niet ongedaan worden gemaakt\nAls je het zeker weet typ \`!ikweethetzeker\`. Na 10 seconde word dit geanuleerd en zal dit ticket niet verwijderd worden.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '!ikweethetzeker', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Je hebt geen !ikweethetzeker getypt, het ticket word niet gesloten').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
+
 
 client.on('message', (message) => {
 
