aedeed038bfd452fcdf2d9d40b5f9534d0553ad9
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2018-05-18 22:19:58 +0200

    update app.js

--- app.js
+++ app.js
@@ -279,7 +279,7 @@ message.channel.send("KAAS? WAAR? IK WIL KAAS!! :cheese:")
   }
   
   if(command === "say") {
-    if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️", "⚙️ Head-Support ⚙️", "bot perms","MC Owner", "MC Owner"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["☄️ Galaxy CEO", "⚙️ Head-Support ⚙️", "bot perms","MC Owner", "MC Owner"].includes(r.name)) )
     return message.reply("Sorry je hebt hier geen perms voor :(");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
@@ -291,7 +291,7 @@ message.channel.send("KAAS? WAAR? IK WIL KAAS!! :cheese:")
   }
 	
 if(command === "set") {
-  if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️"].includes(r.name)) )
+  if(!message.member.roles.some(r=>["☄️ Galaxy CEO"].includes(r.name)) )
   return message.reply("Je hebt hier geen perms voor");
   const sayMessage = args.join(" ");
   message.delete().catch(O_o=>{});
@@ -308,7 +308,7 @@ message.channel.send({ embed: embed });
 
 	
   if(command === "warn") {
-    if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️", "⚙️ Head-Support ⚙️", "bot perms","MC Owner"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["☄️ Galaxy CEO", "⚙️ Head-Support ⚙️", "bot perms","MC Owner"].includes(r.name)) )
     return message.reply("Sorry je hebt hier geen perms voor :(");
     // makes the bot say something and delete the message. As an example, it's open to anyone to use. 
     // To get the "message" itself we join the `args` back into a string with spaces: 
@@ -327,7 +327,7 @@ message.channel.send({ embed: embed });
     // This command must be limited to mods and admins. In this example we just hardcode the role names.
     // Please read on Array.some() to understand this bit: 
     // https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Global_Objects/Array/some?
-    if(!message.member.roles.some(r=>["bot perms","MC Owner", "☄️ Galaxy CEO ☄️", "⚙️ Head-Support ⚙️"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["bot perms","MC Owner", "☄️ Galaxy CEO", "⚙️ Head-Support ⚙️"].includes(r.name)) )
       return message.reply("dacht je nu echt dat jij iemand kon kicken XD");
     
     // Let's first check if we have a member and if we can kick them!
@@ -354,7 +354,7 @@ message.channel.send({ embed: embed });
   if(command === "ban") {
     // Most of this command is identical to kick, except that here we'll only let admins do it.
     // In the real world mods could ban too, but this is just an example, right? ;)
-    if(!message.member.roles.some(r=>["⚙️ Head-Support ⚙️", "☄️ Galaxy CEO ☄️", "bot perms","MC Owner"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["⚙️ Head-Support ⚙️", "☄️ Galaxy CEO", "bot perms","MC Owner"].includes(r.name)) )
       return message.reply("srs !ban dacht je nou echt.......");
     
     let member = message.mentions.members.first();
@@ -372,7 +372,7 @@ message.channel.send({ embed: embed });
   }
   
   if(command === "clear") {
-    if(!message.member.roles.some(r=>["☄️ Galaxy CEO ☄️"].includes(r.name)) )
+    if(!message.member.roles.some(r=>["☄️ Galaxy CEO"].includes(r.name)) )
     return message.reply("je kan geen !clear vraag aan een CEO of head-support om hulp");
     // This command removes all messages from all users in the channel, up to 100.
     
