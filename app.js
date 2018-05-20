d94e5f02cc31a6a89535944385aecc4f79f720d7
XxkuppiexX <37938611+XxkuppiexX@users.noreply.github.com>
2018-05-20 11:22:04 +0200

    update app.js

--- app.js
+++ app.js
@@ -180,6 +180,15 @@ client.on('message', (message) => {
 
 });
 
+client.on('message', (message) => {
+
+  if(message.content.startsWith('http')) {
+    message.delete().catch(O_o=>{}); 
+    message.send('**LET OP!**\n`Ook in de ruimte gelden regels!\ndit is een waarschuwing wegens:\nlinkjes posten zonder toestemming!`')
+  }
+
+});
+
 client.on('guildMemberAdd', member => {
     member.guild.channels.get('428835187456213013').send('🌎 Welkom in de ruimte, ' + member + '!🌎'); 
     member.addRole('427226917738119179')
@@ -209,20 +218,8 @@ client.on("guildMemberAdd", member => {
   });
   
 client.on("message", async message => {
-  // This event will run on every single message received, from any channel or DM.
-  
-  // It's good practice to ignore other bots. This also makes your bot ignore itself
-  // and not get into a spam loop (we call that "botception").
   if(message.author.bot) return;
-  
-  // Also good practice to ignore any message that does not start with our prefix, 
-  // which is set in the configuration file.
   if(message.content.indexOf(config.prefix) !== 0) return;
-  
-  // Here we separate our "command" name, and our "arguments" for the command. 
-  // e.g. if we have the message "+say Is this the real life?" , we'll get the following:
-  // command = say
-  // args = ["Is", "this", "the", "real", "life?"]
   const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
   const command = args.shift().toLowerCase();
   
